6c3afb857972629270d4b140d8f45e216d8ab2e6
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-03-31 03:42:51 +0530

    ready to host

--- dice.js
+++ dice.js
@@ -5,28 +5,40 @@ function roll(){
     const randomnumber = Math.round(Math.random() * (max - min) + min);
    
     const imgElement = document.getElementById('dice-img');
-        switch(randomnumber){
-            case 1:
-                imgElement.scr = 'dice-one.png';
-                break;
-            case 2:
-                imgElement.src='dice-two.png';
-                break;
-            case 3:
-                imgElement.src='dice-three.png';
-                break;
-            case 4:
-                imgElement.src='dice-four.png';
-                break;
-            case 5:
-                imgElement.src="dice-five.png";
-                break;
-            case 5:
-                imgElement.src='dice-six.png';
-                break;
-            default:
-                imgElement.src='dice-default.png';
-        }
+    const resultElement = document.getElementById('result');
+    let resultText;
+
+    switch (randomnumber) {
+        case 1:
+            imgElement.src = 'dice-one.png';
+            resultText = "You rolled a 1!";
+            break;
+        case 2:
+            imgElement.src = 'dice-two.png';
+            resultText = "You rolled a 2!";
+            break;
+        case 3:
+            imgElement.src = 'dice-three.png';
+            resultText = "You rolled a 3!";
+            break;
+        case 4:
+            imgElement.src = 'dice-four.png';
+            resultText = "You rolled a 4!";
+            break;
+        case 5:
+            imgElement.src = 'dice-five.png';
+            resultText = "You rolled a 5!";
+            break;
+        case 6:
+            imgElement.src = 'dice-six.png';
+            resultText = "You rolled a 6!";
+            break;
+        default:
+            imgElement.src = 'dice-default.png';
+            resultText = "Oops! Something went wrong.";
+    }
+
+    resultElement.textContent = resultText;
     imgElement.classList.add("animation");
 
     setTimeout(() =>{
