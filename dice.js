242ede439f9d49aced6f74a64b4205cd05459485
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-03-31 03:16:28 +0530

    ready to host

--- dice.js
+++ dice.js
@@ -3,6 +3,33 @@ function roll(){
     const max = 6;
 
     const randomnumber = Math.round(Math.random() * (max - min) + min);
-    console.log(randomnumber);
    
+    const imgElement = document.getElementById('dice-img');
+        switch(randomnumber){
+            case 1:
+                imgElement.scr = 'dice-one.png';
+                break;
+            case 2:
+                imgElement.src='dice-two.png';
+                break;
+            case 3:
+                imgElement.src='dice-three.png';
+                break;
+            case 4:
+                imgElement.src='dice-four.png';
+                break;
+            case 5:
+                imgElement.src="dice-five.png";
+                break;
+            case 5:
+                imgElement.src='dice-six.png';
+                break;
+            default:
+                imgElement.src='dice-default.png';
+        }
+    imgElement.classList.add("animation");
+
+    setTimeout(() =>{
+        imgElement.classList.remove("animation")}, 1000);
+    
 }
